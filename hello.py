68db1108998adaab5159e592443ddea594676ed1
Eduardo Hitek <eduardohitek@gmail.com>
2014-04-26 10:33:48 -0300

    Baby Steps

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+	print 'Hello there', sys.argv[1]
+
+	print repeat('Yay', False)
+	print repeat('Woo Hoo', True)
+
+def repeat(s, exclain):
+	result = s + s + s
+	if exclain:
+		result = result + '!!!'
+	return result
+
+if __name__ == '__main__':
+	main()
