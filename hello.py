19da21603e0718fd6b67afa93ac8715c530701c7
Eduardo Hitek <eduardohitek@gmail.com>
2014-05-01 22:09:29 -0300

    Refactoring the functional test

--- hello.py
+++ hello.py
@@ -3,16 +3,17 @@
 import sys
 
 def main():
-	print 'Hello there', sys.argv[1]
 
-	print repeat('Yay', False)
-	print repeat('Woo Hoo', True)
+    print('Hello there', sys.argv[1])
+
+    print(repeat('Yay', False))
+    print(repeat('Woo Hoo', True))
 
 def repeat(s, exclain):
-	result = s + s + s
-	if exclain:
-		result = result + '!!!'
-	return result
+    result = s + s + s
+    if exclain:
+        result = result + '!!!'
+    return result
 
 if __name__ == '__main__':
-	main()
+    main()
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -25,6 +25,7 @@ class HomePageTest(TestCase):
 
 		response = home_page(request)
 
+
 		self.assertIn('A new list item', response.content.decode())
 		expected_html = render_to_string(
 			'home.html',
